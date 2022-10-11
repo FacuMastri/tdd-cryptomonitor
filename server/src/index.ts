06314cfa81835d80e18ce099212657fe4d45b149
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-10-11 20:19:18 -0300

    http server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import http from "http";
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
+console.log("Server running at http://localhost:8080/");
