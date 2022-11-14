77c9099a30ffac3b74c9055e51211865a4a520f1
ArmandoCivini <tcivini@gmail.com>
2022-11-14 12:22:36 -0300

    added routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Login from "./screens/login";
-import Dashboard from "./screens/dashboard";
+import Routing from "./routing";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [jwt, setJwt] = useState<string>();
 
   return (
-    <div>
-      <h1>Rostov's Crypto Monitor</h1>
-      {jwt ? <Dashboard jwt={jwt} /> : <Login setJwt={setJwt} />}
-      <ToastContainer />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div>
+        <h1>Rostov's Crypto Monitor</h1>
+        {jwt ? <Routing jwt={jwt} /> : <Login setJwt={setJwt} />}
+        <ToastContainer />
+      </div>
+    </ThemeProvider>
   );
 }
 
