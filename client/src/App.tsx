9e5fded47e29b339df30cb6c144f0c11084fe65b
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-11-14 13:05:33 -0300

    refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,44 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import Login from "./screens/login";
 import Routing from "./routing";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Dashboard from "./screens/dashboard";
+import Rules from "./screens/rules";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
+function Nav() {
+  return (
+    <nav>
+      <h1>Rostov's Crypto Monitor</h1>
+      <div>
+        <Link to="/">Dashboard</Link>
+        <Link to="rules">Rules</Link>
+      </div>
+    </nav>
+  );
+}
+
 function App() {
-  const [jwt, setJwt] = useState<string>();
+  const [jwt, setJwt] = useState<string>("");
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <div>
-        <h1>Rostov's Crypto Monitor</h1>
-        {jwt ? <Routing jwt={jwt} /> : <Login setJwt={setJwt} />}
-        <ToastContainer />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Nav />
+          <Routing jwt={jwt} setJwt={setJwt} />
+        </div>
+      </BrowserRouter>
+      <ToastContainer />
     </ThemeProvider>
   );
 }
--- client/src/routing.tsx
+++ client/src/routing.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./screens/dashboard";
+import Login from "./screens/login";
 import Rules from "./screens/rules";
 
 type Props = {
   jwt: string;
+  setJwt: (jwt: string) => void;
 };
 
-const Routing = ({ jwt }: Props) => {
-    return (
-      <BrowserRouter>
-        <Routes>
-            <Route index element={<Dashboard jwt={jwt}/>} />
-            <Route path="rules" element={<Rules jwt={jwt}/>} />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Routing;
+const Routing = ({ jwt, setJwt }: Props) => {
+  return jwt ? (
+    <Routes>
+      <Route index element={<Dashboard jwt={jwt} />} />
+      <Route path="rules" element={<Rules jwt={jwt} />} />
+    </Routes>
+  ) : (
+    <Login setJwt={setJwt} />
+  );
+};
+
+export default Routing;
