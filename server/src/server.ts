16cdd678f5807095d8f1c3e43102316ff9cf3f5a
efoppiano <efoppiano@fi.uba.ar>
2022-11-13 22:02:19 -0300

    Connect to Binance API

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,14 +9,25 @@ import {
   verifyRulesBody
 } from './controllers';
 import { loginController, verifyJwtController } from './controllers/';
+import BinanceClient, {ExchangeInfoParams} from "./binance/client";
+
+require('dotenv').config();
+
+type BuyOrderParams = {
+  symbol: string;
+  price?: number;
+  quantity?: number;
+}
 
 export default class Server {
   private app: Express;
   private readonly port: number;
+  client: BinanceClient;
 
   constructor(app: Express, port: number) {
     this.app = app;
     this.port = port;
+    this.client = new BinanceClient(process.env.BINANCE_API_KEY??"", process.env.BINANCE_API_SECRET??"");
   }
 
   public start() {
@@ -42,6 +53,62 @@ export default class Server {
       addRulesController
     );
     this.app.get('/rules', verifyJwtHeader, getRulesController);
+
+    // TODO: add jwt verification
+    this.app.get('/binance/exchangeInfo', (req, resp) => {
+      const symbol = req.query.symbol;
+      const symbols = req.query.symbols;
+      const params = {
+        symbol: symbol,
+        symbols: symbols
+      } as ExchangeInfoParams;
+      this.client.getExchangeInfo(params).then((data) => {
+        resp.send(data);
+      });
+    });
+
+    this.app.get('/binance/account', (req, resp) => {
+      this.client.getAccountInfo().then((data) => {
+        resp.send(data);
+      });
+    });
+
+    // These are not meant to be endpoints
+    this.app.get('/binance/buyOrders', (req: Request<BuyOrderParams>, resp) => {
+      const symbol = req.query.symbol;
+      // @ts-ignore
+      const quantity = req.query.quantity as number;
+      // @ts-ignore
+      const price = req.query.price as number;
+
+      if (symbol) {
+        this.client.buy(symbol.toString(), quantity, price).then((data) => {
+          resp.send(data);
+        });
+      } else {
+        resp.send({
+          error: "Symbol is required"
+        });
+      }
+    });
+
+    this.app.get('/binance/sellOrders', (req: Request<BuyOrderParams>, resp) => {
+      const symbol = req.query.symbol;
+      // @ts-ignore
+      const quantity = req.query.quantity as number;
+      // @ts-ignore
+      const price = req.query.price as number;
+
+      if (symbol) {
+        this.client.sell(symbol.toString(), quantity, price).then((data) => {
+          resp.send(data);
+        });
+      } else {
+        resp.send({
+          error: "Symbol is required"
+        });
+      }
+    });
   }
 
   private addHelpers() {
