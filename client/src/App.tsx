281827acf154f3c3311b0b3a4c5d049979098370
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-11-14 15:19:42 -0300

    logout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,8 @@ import jwt_decode from "jwt-decode";
 import Login from "./screens/login";
 import Routing from "./routing";
 import { BrowserRouter, Link } from "react-router-dom";
+import { Avatar } from "@mui/material";
+import BasicMenu from "./util/menu";
 
 const darkTheme = createTheme({
   palette: {
@@ -14,19 +16,36 @@ const darkTheme = createTheme({
   },
 });
 
-function Nav({ admin }: { admin?: boolean }) {
+type NavProps = {
+  admin?: boolean;
+  user?: string;
+  logout: () => void;
+};
+function Nav({ admin, user, logout }: NavProps) {
   return (
     <nav>
-      <h1>Rostov's Crypto Monitor</h1>
-      <div>
-        <Link to="/">Dashboard</Link>
-        {admin ? <Link to="rules">Rules</Link> : ""}
-      </div>
+      <h1>Rostov's Crypto Monitor {user}</h1>
+      {user && (
+        <div>
+          <div>
+            <Link to="/">Dashboard</Link>
+            {admin ? <Link to="rules">Rules</Link> : ""}
+          </div>
+          <BasicMenu
+            items={[
+              { label: user, onClick: () => {} },
+              { label: "Logout", onClick: logout },
+            ]}
+          >
+            <Avatar />
+          </BasicMenu>
+        </div>
+      )}
     </nav>
   );
 }
 
-type UserInfo = { admin?: boolean };
+type UserInfo = { admin?: boolean; user?: string };
 const tokenInfo = (jwt: string): UserInfo => {
   if (!jwt) return {};
   try {
@@ -38,13 +57,16 @@ const tokenInfo = (jwt: string): UserInfo => {
 
 function App() {
   const [jwt, setJwt] = useState<string>("");
-  const { admin } = tokenInfo(jwt);
+  const { user, admin } = tokenInfo(jwt);
+  const logout = () => {
+    setJwt("");
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
         <div>
-          <Nav admin={admin} />
+          <Nav admin={admin} user={user} logout={logout} />
           {jwt ? (
             <Routing jwt={jwt} admin={admin} />
           ) : (
