4bba565d7e0f396b58afaa99683cd98b38410105
fdelu <fdeluca@fi.uba.ar>
2022-11-20 21:07:38 -0300

    Changes on rules screen: Allow to select symbol and market status

--- client/src/screens/rules.tsx
+++ client/src/screens/rules.tsx
@@ -1,10 +1,22 @@
-import TextField from '@mui/material/TextField';
 import { useState, useEffect } from "react";
-import Button from '@mui/material/Button';
 import { rulesAPI, checkOk, intoJson } from "../util/requests";
 import { toast } from "react-toastify";
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import {
+  Stack,
+  Chip,
+  TextField,
+  Button,
+  Select,
+  Typography,
+  MenuItem,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import "../styles/rules.css";
+import { capitalize } from "../util/text";
+
+const MARKET_STATUSES = ["ALZA", "BAJA", "ESTABLE"] as const;
+type MarketStatus = typeof MARKET_STATUSES[number];
 
 type Props = {
   jwt: string;
@@ -13,74 +25,91 @@ type Props = {
 type RulesType = {
   requiredVariables: Array<String>;
   rules: Array<String>;
-}
+};
+
+const fetchRules = async (jwt: string): Promise<any> => {
+  return await await fetch(rulesAPI, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      jwt: jwt,
+    },
+  })
+    .then(checkOk("Couldnt get current rules"))
+    .then(intoJson)
+    .catch((error) => {
+      console.log(error.message ?? error);
+      toast.error(error.message ?? "Error");
+    });
+};
 
 const Rules = ({ jwt }: Props) => {
-
-  const [rules, setRules] = useState("");
-  const [curr_rules, setCurrRules] = useState({
-      requiredVariables: [],
-      rules: [],
-    });
-  // let curr_rules: RulesType = {
-  //   requiredVariables: [],
-  //   rules: [],
-  // };
-  let isLoaded: boolean = false;
+  const [marketStatus, setMarketStatus] = useState<MarketStatus>(
+    MARKET_STATUSES[0]
+  );
+  const [symbol, setSymbol] = useState("");
+  const [rules, setRules] = useState([]);
+  const [requiredVariables, setRequiredVariables] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // fetch data
-    const dataFetch = async () => {
-      const data = await (
-        await   fetch(rulesAPI, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "jwt": jwt,
-          },
-        })
-          .then(checkOk("couldnt get current rules"))
-          .then(intoJson)
-          .catch((error) => {
-            console.log(error.message ?? error);
-            toast.error(error.message ?? "Error");
-          }).then((rules) => {
-            if (typeof rules[0].requiredVariables === 'string' || rules[0].requiredVariables instanceof String) {
-              rules[0].requiredVariables = [rules[0].requiredVariables];
-            }
-            // curr_rules = rules[0];
-            setCurrRules(rules[0]);
-            isLoaded = true;
-          }));
-    };
-
-
-    dataFetch();
+    fetchRules(jwt).then((rules) => {
+      setRequiredVariables(rules.requiredVariables);
+      setRules(rules.rules);
+    });
   }, []);
 
-    const sendRules = () => {
-      console.log(rules);
-      console.log(curr_rules?.requiredVariables);
-      //TODO post rules
-    };
+  const sendRules = () => {
+    //TODO post rules
+  };
 
   return (
     <section>
-      <h1>Rules</h1>
+      <Typography variant="h3">Rules</Typography>
+      <div className="options">
+        <Typography>Rule Options</Typography>
+        <div className="form">
+          <FormControl className="formInput">
+            <InputLabel>Market Status</InputLabel>
+            <Select
+              value={marketStatus}
+              onChange={(e) => setMarketStatus(e.target.value as MarketStatus)}
+              label="Market Status"
+            >
+              {MARKET_STATUSES.map((status) => (
+                <MenuItem value={status} key={status}>
+                  {capitalize(status)}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl className="formInput">
+            <TextField
+              label="Symbol"
+              value={symbol}
+              onChange={(e) => setSymbol(e.target.value)}
+            />
+          </FormControl>
+        </div>
+      </div>
 
       <h2>Required Variables</h2>
 
-      <Stack direction="row" spacing={1} style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-        {curr_rules.requiredVariables ? curr_rules.requiredVariables.map((algo, idx) => {
-          return <Chip key={idx} label={algo} variant="outlined" />;
-        }) : null}
+      <Stack
+        direction="row"
+        spacing={1}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {curr_rules?.requiredVariables
+          ? curr_rules.requiredVariables.map((algo, idx) => {
+              return <Chip key={idx} label={algo} variant="outlined" />;
+            })
+          : null}
       </Stack>
-
-      <h2>Rules</h2>
 
       <table>
         <thead>
@@ -103,16 +132,20 @@ const Rules = ({ jwt }: Props) => {
 
       <h2>Add Rule</h2>
       <TextField
-          id="rules-field"
-          label="Add Rules"
-          multiline
-          rows={4}
-          style={{"width": "100%"}}
-          onChange={(e) => {
-            setRules(e.target.value);
+        id="rules-field"
+        label="Add Rules"
+        multiline
+        rows={4}
+        style={{ width: "100%" }}
+        onChange={(e) => {
+          setRules(e.target.value);
         }}
       />
-      <Button variant="contained" onClick={sendRules} style={{"float": "right", "marginTop": "10px"}}>
+      <Button
+        variant="contained"
+        onClick={sendRules}
+        style={{ float: "right", marginTop: "10px" }}
+      >
         Set Rules
       </Button>
       <footer>{jwt}</footer>
