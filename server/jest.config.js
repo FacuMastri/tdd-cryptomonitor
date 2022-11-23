726508878422941644c9266f216fbdcc67d42307
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-11-23 15:02:54 -0300

    jest config
    skip coverage for
    - service wrappers
    - controllers

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/node_modules/**',
+    '!**/services/**',
+    '!**/controllers/**'
+  ],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   testMatch: ['**/*.test.ts']
 };
