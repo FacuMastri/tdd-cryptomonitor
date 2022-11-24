25c7642fe6a1d82392371f12534da3e82d4317cf
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-11-24 00:54:21 -0300

    api-docs init
    - user apis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Express } from 'express';
 import Server from './server';
 import { PORT } from './config';
+import swaggerUi from 'swagger-ui-express';
+import openapiSpecification from './openapi.json';
 
 const app: Express = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 const server: Server = new Server(app, Number(PORT));
 server.start();
